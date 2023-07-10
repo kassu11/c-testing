31c3f5a255ee6653ffcf1f9b7463efdb4b5aa914
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-07-10 14:57:28 +0300

    chapter 13 exercise 1

---
+++ viope/chapter-13/exercise-1/returning-a-pointer-from-a-function.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int *address_of_smallest_value(int *numbers, int size);
+
+int main(void)
+{
+	int numbers[] = {21, 55, 5, 3, 43};
+	int size = 5;
+	int *smallest = address_of_smallest_value(numbers, size);
+	printf("The smallest number in the array is: %d", *smallest);
+
+	return 0;
+}
+
+int *address_of_smallest_value(int *numbers, int size)
+{
+	int *smallest = numbers;
+	for (int i = 0; i < size; i++)
+	{
+		if (numbers[i] < *smallest)
+		{
+			smallest = &numbers[i];
+		}
+	}
+	return smallest;
+}
