a31487f1b629eb1f9ca171f24a3106d5aadd7f10
kassu11 <61124141+kassu11@users.noreply.github.com>
2023-07-10 14:16:44 +0300

    chapter 12 exercise 1

---
+++ viope/chapter-12/exercise-1/changing-a-number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void change_number(int x);
+
+int main()
+{
+	int x;
+	printf("Enter the number x: ");
+	scanf("%d", &x);
+	printf("In the main program: x = %d\n", x);
+	change_number(x);
+	printf("In the main program: x = %d\n", x);
+}
+
+void change_number(int x)
+{
+	x = x + 3;
+	printf("In the subroutine: x = %d\n", x);
+}
